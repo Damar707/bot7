d1d62e126e42bb40beec5026c67fb1d1ad695cc7
Damar707 <81214793+Damar707@users.noreply.github.com>
2023-06-01 20:54:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,27 +10,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['6283867228380'] //ur owner number
+global.ownernomer = "6283867228380" //ur owner number2
+global.ownername = "Damzz25" //ur owner name
+global.ytname = "YT: mrk" //ur yt chanel name
+global.socialm = "GitHub: Damar707" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V7"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/ecl-JDVDz3E"
+global.botname = "Damzz7"
+global.ownernumber = '6283867228380'
+global.ownername = 'Damzz'
+global.ownerNumber = ["62838672283803@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@mrk"
+global.websitex = "https://youtu.be/Ev-eAT2P_R4"
 global.wagc = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
 global.themeemoji = '🥀'
-global.wm = "Xeon Bot Inc."
+global.wm = "Damz Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD7' //script link
 global.packname = "Sticker By"
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Damzz Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213"
+global.creator = "6283867228380@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.hituet = 0
 
